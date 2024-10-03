dd89d8a30d2e67650431739affb6f645a3a44fab
Lucas Eduardo <lesds2001@gmail.com>
2024-10-02 23:43:39 -0300

    Adicionando salvamento de tarefa ao aync storage

--- src/components/taskScreens/Content/index.tsx
+++ src/components/taskScreens/Content/index.tsx
@@ -1,10 +1,14 @@
 import { TextInput, Text, View, TouchableOpacity, Button } from 'react-native';
 import * as C from './style';
-import imgBack from './../../../assets/back-Button.svg';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { taskContent } from '../../../utils/types';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import { useState } from 'react';
 
 export const Content = () => {
+
+
+    const [teste, setTeste] = useState(false);
 
     const [titleTask, setTitleTask] = useState<string>("");
     const [descricao, setDescricao] = useState<string>("");
@@ -26,28 +30,59 @@ export const Content = () => {
         setShowDate(true);
         setDate(currentDate);
     };
+   
+    const storeData = async () => {
+
+        
+        if (titleTask !== "" && descricao !== "" && showDate) {
+
+            const value: taskContent = {
+                title: titleTask,
+                descricao: descricao,
+                date: date
+            };
+            try {
+                await AsyncStorage.setItem('task', JSON.stringify(value));
+
+            } catch (e) {
+                // saving error
+                
+            setTeste(true);
+            }finally{
+                setTitleTask("")
+                setDescricao("")
+                setShowDate(false)
+            }
+        }
+    };
 
     return (
         <C.Container>
-
+            
             <C.InputBox>
                 <C.TextInp>Titulo da Tarefa:</C.TextInp>
                 <C.Input
                     height={56}
                     onChangeText={t => setTitleTask(t)}
+                    value={titleTask}
                 />
             </C.InputBox>
+
+            
 
             <C.InputBox>
                 <C.TextInp>Descrição:</C.TextInp>
                 <C.Input
                     height={170}
                     onChangeText={t => setDescricao(t)}
-                    textAlignVertical="top"
+                    textAlignVertical="top"                    
+                    value={descricao}
                 />
             </C.InputBox>
 
-            <View style={[{"marginBottom": 15}]}>
+           
+
+            <View style={[{ "marginBottom": 15 }]}>
 
                 <C.TextInp>Data</C.TextInp>
                 <C.InputDate onPress={() => setShow(true)}>
@@ -59,13 +94,24 @@ export const Content = () => {
                             onChange={onChange}
                         />
                     )}
-                    <Text style={[{color: "#fff"}, {fontSize: 16}]}>{showDate ? formatDate(date) : 'Selecione a Data'}</Text>
+                    <Text style={[{ color: "#fff" }, { fontSize: 16 }]}>{showDate ? formatDate(date) : 'Selecione a Data'}</Text>
                 </C.InputDate >
             </View>
+            {
+                teste &&
+                <Text style={[{color: "#fff"}]}>NÃO FOI POSSIVEL ENVIAR, TENTE NOVAMENTE</Text>
+            }
+            
 
-            <C.ButtonTask>
-                <Text style={[{color: "#fff"}, {fontSize: 19}]}>Criar Tarefa</Text>
+            <C.ButtonTask onPress={() => storeData()}>
+                <Text style={[{ color: "#fff" }, { fontSize: 19 }]}>Criar Tarefa</Text>
+
             </C.ButtonTask>
+
+
+            
+
+
 
 
         </C.Container>
