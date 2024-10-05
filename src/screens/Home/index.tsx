6e9aeb50535242e1009259aab6a95eab2ba87353
Pedro Henrique <65197732+Nickdyegars@users.noreply.github.com>
2024-10-05 11:43:33 -0300

    Implementação da barra de busca
    Implementação da barra de busca
    Melhora no algoritmo de contagem dos cards
    Bug do check não corrigido

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -7,116 +7,113 @@ import { Task } from '../../components/Task';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { taskContent } from '../../utils/types';
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
 import { NewTaskScreenNavigationProp } from '../../utils/types';
 import { PopupEdit } from '../../components/PopupEdit';
+import { useCallback } from 'react';
 
 export function Home() {
 
   const [taskData, setTaskData] = useState<taskContent[]>(null);
+  const [originalTaskData, setOriginalTaskData] = useState<taskContent[]>(null);  // Tarefas originais
   const [amountTask, setAmountTask] = useState<number>(0);
   const [openTask, setOpenTask] = useState<number>(0);
   const [closedTask, setClosedTask] = useState<number>(0);
   const [showModal, setShowModal] = useState<boolean>(false);
   const [taskEdit, setTaskEdit] = useState<taskContent>(null);
+  const [searchText, setSearchText] = useState<string>("");
   const navigation = useNavigation<NewTaskScreenNavigationProp>();
-  
 
   const { height, width } = Dimensions.get('window');
 
+  // Função para buscar as tarefas
+  const getData = async (): Promise<taskContent[]> => {
+    try {
+      const taskData = await AsyncStorage.getItem("task");
+      const taskItemsData = taskData ? JSON.parse(taskData) : [];
+
+      return taskItemsData;
+    } catch {
+      console.log("Erro ao recuperar dados");
+    }
+  };
+
+  const countOpenTasks = () => {
+    let totalTasks = taskData.length;
+    let totalTasksOpen = taskData.filter((task) => task.status === false).length;
+    let totalTasksCompleted = taskData.filter((task) => task.status === true).length;
+
+    setClosedTask(totalTasksCompleted);
+    setOpenTask(totalTasksOpen);
+    setAmountTask(totalTasks);
+  }
+
   useEffect(() => {
-    const getData = async (): Promise<taskContent[]> => {
-      try {
-        const taskData = await AsyncStorage.getItem("task");
+    countOpenTasks();
+  })
 
-        const taskItemsData = taskData ? JSON.parse(taskData) : [];
-        setAmountTask(taskItemsData.length)
+  // UseFocusEffect para garantir que os dados sejam buscados ao focar a tela
+  useFocusEffect(
+    useCallback(() => {
+      const fetchTaskData = async () => {
+        const tasks = await getData();
+        setAmountTask(tasks.length);
+        setTaskData(tasks);
+        setOriginalTaskData(tasks);
+      };
 
-        let closed: number = 0;
-        let open: number = 0;
-
-        for (let i in taskItemsData) {
-          if (taskItemsData[i].status === true) {
-            closed += 1;
-          } else {
-            open += 1;
-          }
-
-          setClosedTask(closed);
-          setOpenTask(open)
-
-        }
-
-        return taskItemsData;
-      } catch {
-        console.log("Erro ao recuperar dados")
-      }
-    }
-
-    const fetchTaskData = async () => {
-      setTaskData(await getData());
-    }
-
-    fetchTaskData();
-
-
-  }, [taskData]);
-
+      fetchTaskData();
+    }, [])
+  );
 
   const handleDeleteTask = async (id: string) => {
-
     const taskData = await AsyncStorage.getItem("task");
     const taskItemsData = taskData ? JSON.parse(taskData) : [];
     const taskWithOutDeleted = taskItemsData.filter(task => task.id !== id);
 
     await AsyncStorage.setItem('task', JSON.stringify(taskWithOutDeleted));
 
-    let open = 0;
-    let closed = 0;
-
-    for (let i in taskWithOutDeleted) {
-      if (taskItemsData[i].status) {
-        open += 1;
-      } else {
-        closed += 1;
-      }
-    }
-    setClosedTask(closed);
-    setOpenTask(open)
+    // countOpenTasks();
 
     setTaskData(taskWithOutDeleted);
   }
 
   const handleChageStatus = async (id: string, newStatus: boolean) => {
-
     const taskData = await AsyncStorage.getItem("task");
     const taskItemsData = taskData ? JSON.parse(taskData) : [];
     const tasks = taskItemsData.map(task =>
       task.id === id ? { ...task, status: newStatus } : task
     )
 
-
     await AsyncStorage.setItem('task', JSON.stringify(tasks));
 
     setTaskData(tasks);
-
   }
 
   const handleNavigation = () => {
-
-    navigation.navigate('NewTask')
-
+    navigation.navigate('NewTask');
   }
 
-  const handleAddTask = () => {
+  const handleSearch = (text: string) => {
+    setSearchText(text);
 
-  }
+    if (text.trim() === "") {
+      setTaskData(originalTaskData);
+      return;
+    }
 
-  const setTaskText = () => {
+    const filteredTasks = originalTaskData.filter(task =>
+      task.title.toLowerCase().includes(text.toLowerCase())
+    );
 
-  }
+    const remainingTasks = originalTaskData.filter(
+      task => !task.title.toLowerCase().includes(text.toLowerCase())
+    );
 
-  const handleCloseModal = () =>{
+    setTaskData([...filteredTasks, ...remainingTasks]);
+  };
+
+  const handleCloseModal = () => {
     setShowModal(false);
     setTaskEdit(null);
   }
@@ -124,15 +121,14 @@ export function Home() {
   const handleShowModal = async (id: string) => {
     const taskData = await AsyncStorage.getItem("task");
     const taskItemsData = taskData ? JSON.parse(taskData) : [];
-    
-    for(let i in taskItemsData){
-      if(taskItemsData[i].id === id){
+
+    for (let i in taskItemsData) {
+      if (taskItemsData[i].id === id) {
         setTaskEdit(taskItemsData[i]);
       }
     }
 
     setShowModal(true);
-
   }
 
   const styles = StyleSheet.create({
@@ -159,33 +155,22 @@ export function Home() {
       zIndex: 2,
       alignItems: "center",
       justifyContent: "center",
-
     }
   });
 
   return (
     <View style={styles.container}>
-
-      { showModal &&
+      {showModal &&
         <View style={styles.containerPopUp}>
-          <PopupEdit taskData={taskEdit} closeModal={handleCloseModal}/>
+          <PopupEdit taskData={taskEdit} closeModal={handleCloseModal} />
         </View>
       }
 
-
-      <InputAddTask onPress={handleAddTask} onChangeText={setTaskText} value={''} />
+      <InputAddTask onChangeText={handleSearch} value={searchText} />
       <View style={{ flexDirection: 'row', gap: 16, marginTop: 18 }}>
-        <CardNumber
-          title={'Cadastradas'}
-          num={amountTask} color={'#1E1E1E'} />
-        <CardNumber
-          title={'Em aberto'}
-          num={openTask} color={'#FA9216'}
-        />
-        <CardNumber
-          title={'Finalizadas'}
-          num={closedTask} color={'#11ad8b'}
-        />
+        <CardNumber title={'Cadastradas'} num={amountTask} color={'#1E1E1E'} />
+        <CardNumber title={'Em aberto'} num={openTask} color={'#FA9216'} />
+        <CardNumber title={'Finalizadas'} num={closedTask} color={'#11ad8b'} />
       </View>
 
       {taskData &&
@@ -205,9 +190,6 @@ export function Home() {
       <TouchableOpacity onPress={handleNavigation} style={styles.addButton}>
         <Ionicons name="add-circle" size={60} color="#FA9216" />
       </TouchableOpacity>
-
-
-
     </View>
   );
 }
